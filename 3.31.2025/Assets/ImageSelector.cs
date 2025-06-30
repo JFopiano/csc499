c3b11678e43680909ac09fd9a207013ae05b1b82
Jason Fopiano <144917581+JFopiano@users.noreply.github.com>
2025-06-30 12:44:52 -0400

    Revert "Finished (Hopefully) Classroom 4"
    This reverts commit 224695a9e12b847c28c4b87cdb1a0d5115001b44.

--- 3.31.2025/Assets/ImageSelector.cs
+++ 3.31.2025/Assets/ImageSelector.cs
@@ -1,46 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class ImageSelector : MonoBehaviour
 {
-
-    public TMP_Dropdown dropdown;
-    public RawImage imageDisplay;
-    public Button showButton;
-
-    private List<string> imageNames = new List<string>()
-    {
-        "AreaMeanColor", "CompactnessMeanColor", "ConcavePointMeanColor", "ConcavityMeanColor", "FractalDimensionMeanColor", "PerimeterMeanColor", "RadiusMeanColor", "SmoothnessMeanColor", "SymmetryMeanColor", "TextureMeanColor"
-    };
-
-
     // Start is called before the first frame update
     void Start()
     {
-        dropdown.ClearOptions();
-        dropdown.AddOptions(imageNames);
-
-        showButton.onClick.AddListener(DisplaySelectedImage);
-
+        
     }
 
-    void DisplaySelectedImage()
+    // Update is called once per frame
+    void Update()
     {
-        string selectedImageName = imageNames[dropdown.value];
-
-        Texture2D tex = Resources.Load<Texture2D>("Images/" + selectedImageName);
-
-        if (tex != null)
-        {
-            imageDisplay.texture = tex;
-            imageDisplay.SetNativeSize();
-        }
-        else
-        {
-            Debug.LogError("Image Not Found: " + selectedImageName);
-        }
+        
     }
 }
